d610ffc7c2e0e2b61e4b7124f9db9d6799446374
Veronique T <veroniqueconcordia@gmail.com>
2024-10-13 15:42:05 -0400

    Trying to fix issues

--- soen342/src/main/java/com/concordia/soen342/Instructor.java
+++ soen342/src/main/java/com/concordia/soen342/Instructor.java
@@ -9,4 +9,10 @@ public class Instructor extends Person{
 
     //operations
     public Instructor(){}
+
+    public Instructor(String n, long pn, String pw, Vector<String> s, Vector<String> a){
+        super(n,pn,pw,"INSTRUCTOR");
+        this.specialization = s;
+        this.availableCities = a;
+    }
 }
