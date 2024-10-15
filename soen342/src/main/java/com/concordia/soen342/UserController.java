8370a3b6bc7b135f979a48256701a51687edecba
irizhukova <ira.zhu.2003@gmail.com>
2024-10-15 08:22:38 -0400

    frontend files

--- soen342/src/main/java/com/concordia/soen342/UserController.java
+++ soen342/src/main/java/com/concordia/soen342/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
@@ -14,6 +15,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/users")
+@CrossOrigin(origins = "*")
 public class UserController {
     @Autowired
     private UserService userService;
