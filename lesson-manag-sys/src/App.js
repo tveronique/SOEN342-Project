9f88e5844f62a51331ba4af3530ebc4d38524be6
Veronique T <veroniqueconcordia@gmail.com>
2024-10-16 16:35:53 -0400

    Started the styling of the webapp - added the router, created pages, created header and navbar and started desiging the home page

--- lesson-manag-sys/src/App.js
+++ lesson-manag-sys/src/App.js
@@ -1,6 +1,15 @@
 import './App.css';
 import api from './api/axiosConfig';
 import {useState,useEffect} from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import NavBar from './components/NavBar'
+import Home from './pages/Home' 
+import SignUp from './pages/SignUp' 
+import LogIn from './pages/LogIn'
+import Account from './pages/Account' 
+import AdminDashboard from './pages/AdminDashboard'  
+import InstructorDashboard from './pages/InstructorDashboard'
 
 function App() {
 
@@ -25,6 +34,21 @@ useEffect(() => {
 
   return (
     <div className="App">
+      <Header />
+      <BrowserRouter>
+      <NavBar />
+        <Routes>
+          {/* set the home page to the default page */}
+          <Route index element = {<Home />} /> 
+          {/* rest of the paths */}
+          <Route path="/home" element = {<Home />} />
+          <Route path="/signup" element = {<SignUp />} />
+          <Route path="/login" element = {<LogIn />} />
+          <Route path="/account" element = {<Account />} />
+          <Route path="/admindash" element = {<AdminDashboard />} />
+          <Route path="/instructordash" element = {<InstructorDashboard />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
