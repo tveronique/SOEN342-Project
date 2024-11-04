dcff78a18086f6e250ead154747b5ec221e3babf
Veronique T <veroniqueconcordia@gmail.com>
2024-11-04 03:29:43 -0500

    completed the log in

--- lesson-manag-sys/src/components/NavBar.js
+++ lesson-manag-sys/src/components/NavBar.js
@@ -1,19 +1,43 @@
-import React from 'react'
-import { Container, Nav, Navbar } from 'react-bootstrap'
-import '../App.css'
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import '../App.css';
+import { useAuth } from '../context/AuthContext';
 
 const NavBar = () => {
+  const { role, logout } = useAuth();
+
+  const handleLogout = () => {
+    logout();
+    navigate('/home');
+  };
+
   return (
     <Navbar bg="light" data-bs-theme="light" className="navbar-custom">
-        <Container>
-          <Navbar.Brand href="#home">Actify</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="/" className="nav-item">View Offerings</Nav.Link>
-            <Nav.Link href="/login" className="nav-item">Log In</Nav.Link>
-            <Nav.Link href="/signup" className="nav-item">Register</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
+      <Container>
+        <Navbar.Brand href="/home">Actify</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="/" className="nav-item">View Offerings</Nav.Link>
+          {role ? ( // Check if a user is logged in 
+            <>
+              <Nav.Link href="/account" className="nav-item">My Account</Nav.Link>
+              {role === 'ADMIN' && ( // If user is admin
+                <Nav.Link href="/admindash" className="nav-item">Admin Dashboard</Nav.Link>
+              )}
+              {role === 'INSTRUCTOR' && ( // If user is instructor
+                <Nav.Link href="/instructordash" className="nav-item">Instructor Dashboard</Nav.Link>
+              )}
+              <Nav.Link onClick={handleLogout} className="nav-item">Log Out</Nav.Link>
+            </>
+          ) : (
+            <>
+              <Nav.Link href="/login" className="nav-item">Log In</Nav.Link>
+              <Nav.Link href="/signup" className="nav-item">Register</Nav.Link>
+            </>
+          )}
+        </Nav>
+      </Container>
+    </Navbar>
   );
 };
+
 export default NavBar;
