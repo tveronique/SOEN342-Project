fac7157261c36768d78624689cafc59eefa36483
irizhukova <ira.zhu.2003@gmail.com>
2024-11-09 16:16:30 -0500

    my account page modif

---
+++ lesson-manag-sys/src/pages/Account.js
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import useFetchBookings from '../hooks/useFetchBookings';
+import BookingButton from '../components/BookingButton';
+
+function Account() {
+  const { bookings, error } = useFetchBookings();
+  console.log("bookings:", bookings)
+  const [detailedBookings, setDetailedBookings] = useState([]);
+
+  useEffect(() => {
+
+    const fetchSelectedBookings = async () => {
+      const bookingDetailsPromises = bookings.map(async (booking) => {
+        try {      
+
+          // Fetch offering details
+          const offeringResponse = await axios.get(`/api/offerings/${booking.offeringId}`);
+          const offeringData = offeringResponse.data;
+          console.log("offering data:", offeringData);
+
+          // Fetch instructor details
+          const instructorResponse = await axios.get(`/api/users/phone/${booking.instructorPhoneNumber}`);
+          const instructorData = instructorResponse.data;
+
+          return { ...booking, offering: offeringData, instructor: instructorData };
+        } catch (error) {
+          console.error('Error fetching details:', error);
+          return { ...booking, offering: null, instructor: null };
+        }
+      });
+      // Wait for all booking details to be fetched
+      const bookingDetails = await Promise.all(bookingDetailsPromises);
+      setDetailedBookings(bookingDetails);
+      console.log("booking details:", bookingDetails);
+    };
+
+    fetchSelectedBookings();
+  }, [bookings]);
+
+  if (error) {
+    return <div>Error loading bookings: {error.message}</div>;
+  }
+
+  return (
+    <div>
+      <h2>Offerings</h2>
+      {detailedBookings.map((booking) => (
+        <div key={booking.id} className="viewOfferings">
+          <h3>{booking.offering.lesson.type} LESSON</h3>
+          <p>
+            <b>Location</b>: {booking.offering?.location.name} {booking.offering?.location.space.type}, {booking.offering?.location.city} <br />
+            <b>Day</b>: {booking.offering?.location.schedule.day}s from {booking.offering?.location.schedule.startDate} until {booking.offering?.location.schedule.endDate}<br />
+            <b>Time</b>: {booking.offering?.location.schedule.startTime} - {booking.offering?.location.schedule.endTime} <br />
+            <b>{booking.offering?.lesson.private ? "Private lesson" : "Group lesson"}</b> <br></br>
+            <b>Instructor</b>: {booking.instructor?.name} <br></br>
+            <b>Instructor Phone</b>: {booking.instructor?.phoneNumber}
+          </p> {console.log(booking.id)}
+          <p className='text-center'><BookingButton booking={booking} /> </p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Account;
